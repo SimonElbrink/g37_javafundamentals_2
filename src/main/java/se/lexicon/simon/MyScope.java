91089f81e3c0487ec723a39c8b9addf384bc9c91
Simon Elbrink <simon.elbrink@gmail.com>
2021-06-02 13:17:00 +0200

    Scopes Update

--- src/main/java/se/lexicon/simon/MyScope.java
+++ src/main/java/se/lexicon/simon/MyScope.java
@@ -5,35 +5,38 @@ public class MyScope {
     int number = 50;
     static int num = 10;
 
-
     public static void main(String[] args) {
-
         int num = 0;
 
+        foo();
+        MyScope.foo();// Same as "foo()" in this situation, more specific or Targeted.
+    }
+
+    public static void foo(){
+        int num = 90;
 
         System.out.println(num);
         System.out.println(MyScope.num);
+        System.out.println("Local Variable : " + num);
+        System.out.println("MyScope.num : " + MyScope.num);
+    }
 
-        foo();
-        MyScope.foo();
+    public void foo1(){
+        int number = 40;
+
+        System.out.println("Local Variable : " + number);
+        System.out.println("This.number : " + this.number);
+    }
+
+    // Empty Instance Block
+    {
 
     }
 
+    // Empty Static Block
     static {
 
     }
 
-    public static void foo(){
 
-        int num = 90;
-    }
-
-    public void foo1(){
-
-        int number = 40;
-
-        System.out.println(number);
-        System.out.println(this.number);
-
-    }
 }
